e10f6ba66b3220cce7385e43a6a2f901293ed0ed
Satya Tamby <tambysatya@gmail.com>
2026-09-13 14:36:00 +0200

    always peminstall

--- tools/secrets-generator/lib/installer.nix
+++ tools/secrets-generator/lib/installer.nix
@@ -37,7 +37,7 @@ let
         in ''
             ${installFile crt owner owner "0400"} 
             ${installFile key owner owner "0400"} 
-            ${if owner == "haproxy" then peminstall else ""}
+            ${peminstall}
         '';
 
     mkPgPass = 
