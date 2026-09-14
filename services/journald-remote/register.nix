865abe928b34c187bd45d470ae48b0d0c5d36ef2
Satya Tamby <tambysatya@gmail.com>
2026-09-14 11:40:15 +0200

    disabled ssl secrets in journald

--- services/journald-remote/register.nix
+++ services/journald-remote/register.nix
@@ -18,14 +18,14 @@ let
             }
         ];
     };
-    ssl = {inherit hostname owner reload;};
+    #ssl = {inherit hostname owner reload;};
 in {
 infra.services.journald-remote = {
     users = {
         "systemd-journal-remote" = {service ="systemd-journal-remote"; uid=10008;};
         "systemd-journal-upload" = {service ="systemd-journal-upload"; uid=10009;}; #clients
     };
-    store.sslCertificates = [ssl];
+    #store.sslCertificates = [ssl];
     inherit endpoints;
 };
 }
