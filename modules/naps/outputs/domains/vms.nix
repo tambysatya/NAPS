d70e1cff3c14b81b093d9ae2d16ee8d4a4c3f253
Satya Tamby <tambysatya@gmail.com>
2026-09-18 09:58:37 +0200

    moved net conf from smbios processors to baseboard

--- modules/naps/outputs/domains/vms.nix
+++ modules/naps/outputs/domains/vms.nix
@@ -15,7 +15,7 @@ let
                   {smbios = {
                     system = {entry = [{name = "serial"; value = vmname;}];};
                     bios = {entry = [{name = "serial"; value = vmconf.ip;}];};
-                    processor = {entry = [{name = "serial"; value = vmconf.gateway;}];};
+                    base_board = {entry = [{name = "serial"; value = vmconf.gateway;}];};
                     #chassis = {entry = [{name = "serial"; value = token;}];}; #need to be added purely (maybe using an app)
                     chassis = {entry = [{name = "serial"; value = "${lib.toUpper vmname}_TOKEN";}];};  # TO REPLACE WITH SED
                     };
