c50f576fdd09aebb714e29749410d63400cfa630
Satya Tamby <tambysatya@gmail.com>
2026-09-21 08:33:38 +0200

    removed dependency in templates

--- templates/flake.nix
+++ templates/flake.nix
@@ -23,7 +23,7 @@ description = "Automatic generation of Terraform and NixOS configurations for a 
         };
     in  naps.lib.exposeApps args // {
         hydraJobs = {
-            inherit pkgs;
+            inherit (self) checks;
         };
         nixosConfigurations = naps.lib.compileNixos args; 
         terranix = naps.lib.compileTerranix args;
